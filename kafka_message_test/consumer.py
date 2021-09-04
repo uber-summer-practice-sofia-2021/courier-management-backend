c77a7f3e5792a5c884e83b0422d363c3c835a708
Pavel Sarlov <sarlovpavel@gmail.com>
2021-09-04 04:01:42 -0400

    Format with py-black

--- kafka_message_test/consumer.py
+++ kafka_message_test/consumer.py
@@ -1,8 +1,12 @@
 from kafka import KafkaConsumer
 import json
 
-KAFKA_TOPIC = 'trips'
+KAFKA_TOPIC = "trips"
 
-consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers='0.0.0.0:9092', value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+consumer = KafkaConsumer(
+    KAFKA_TOPIC,
+    bootstrap_servers="0.0.0.0:9092",
+    value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+)
 for message in consumer:
     print(message.value)
--- kafka_message_test/producer.py
+++ kafka_message_test/producer.py
@@ -1,10 +1,13 @@
 from kafka import KafkaProducer
 import json
 
-KAFKA_TOPIC = 'trips'
-KAFKA_SERVERS = ['0.0.0.0:9092']
+KAFKA_TOPIC = "trips"
+KAFKA_SERVERS = ["0.0.0.0:9092"]
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVERS, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_SERVERS,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 
 producer.send(KAFKA_TOPIC, {"test": "data"})
 producer.flush()
