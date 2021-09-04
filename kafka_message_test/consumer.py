ebba6f14c3b39dfe84c9cdea056399535d397411
Pavel Sarlov <sarlovpavel@gmail.com>
2021-09-04 03:46:13 -0400

    Add kafka to docker and setup simple mockup

---
+++ kafka_message_test/consumer.py
@@ -0,0 +1,8 @@
+from kafka import KafkaConsumer
+import json
+
+KAFKA_TOPIC = 'trips'
+
+consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers='0.0.0.0:9092', value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+for message in consumer:
+    print(message.value)
